da425786862b6f5bc8a3e75f688c91990746ec5e
Dino1984WB <williamkbukowkidev@gmail.com>
2023-05-30 05:10:38 -0400

    second commit

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,15 @@
 #this is a basic program to be run in a docker container and demonstrate system logging
 
+
+#prints helloworld once
 def helloworld():
     print("hello, world!")
 
+#repeatedly calls helloworld 10 times
 def repeat():
     num = range(10)
     for x in num:
         helloworld()
 
+#repeat function call
 repeat()
